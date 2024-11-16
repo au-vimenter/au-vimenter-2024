e74de55c0d0235a1030c864ff079b9e2f43c0b1c
devstefancho <skku7714@gmail.com>
2024-11-16 14:05:46 +0900

    feat: mobile 페이지

--- src/components/EventSupporter.jsx
+++ src/components/EventSupporter.jsx
@@ -1,26 +1,10 @@
+import styles from "../styles/EventSupporter.module.css";
+
 const EventSupporter = () => {
   return (
-    <div
-      style={{
-        position: "fixed",
-        bottom: "30px",
-        left: "50%",
-        transform: "translateX(-50%)",
-        display: "flex",
-        gap: 10,
-        alignItems: "center",
-        justifyContent: "center",
-        width: "max-content",
-      }}
-    >
+    <div className={styles.container}>
       <div>장소협찬: 리턴제로</div>
-      <div
-        style={{
-          height: 20,
-          background: "var(--color-text-blue)",
-          width: 2,
-        }}
-      ></div>
+      <div className={styles.divider}></div>
       <div>다과협찬: LG 유플러스 아이들나라</div>
     </div>
   );
--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -1,4 +1,31 @@
+import EventDescription from "./EventDescription";
+import EventEntrollButton from "./EventEntrollButton";
+import EventInfo from "./EventInfo";
+import EventSupporter from "./EventSupporter";
+import Header from "./Header";
+import VimEnterTitle from "./VimEnterTitle";
+
 const Mobile = () => {
-  return <h1>Mobile</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Header />
+      <div className="space-110"></div>
+      <VimEnterTitle />
+      <div className="space-70"></div>
+      <EventDescription />
+      <div className="space-60"></div>
+      <EventInfo />
+      <div className="space-50"></div>
+      <EventSupporter />
+      <EventEntrollButton />
+    </div>
+  );
 };
 export default Mobile;
