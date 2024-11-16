70338f8c2cfb134ad64ccf8835d13064606c4e0c
devstefancho <skku7714@gmail.com>
2024-11-16 23:10:55 +0900

    chore: header 영역 style 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,18 @@ import styles from "../styles/Header.module.css";
 
 const Header = () => {
   return (
-    <nav className={styles.navbar}>
-      {links.map((link) => {
-        return (
-          <a key={link.url} href={link.url} target="_blank">
-            <img src={link.imgSrc} alt={link.url} />
-            <span>{link.name}</span>
-          </a>
-        );
-      })}
-    </nav>
+    <div className={styles.container}>
+      <nav className={styles.navbar}>
+        {links.map((link) => {
+          return (
+            <a key={link.url} href={link.url} target="_blank">
+              <img src={link.imgSrc} alt={link.url} />
+              <span>{link.name}</span>
+            </a>
+          );
+        })}
+      </nav>
+    </div>
   );
 };
 
